2c6fb5f2a7fafaa5cdabcc015d43d8c06ac71eb0
Sam Saccone <samccone@gmail.com>
2017-11-05 14:05:12 -0800

    Remove sticky container and use vanilla sticky

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,17 +1,16 @@
 /* eslint-disable no-unused-vars */
-import React from "react"
-/* eslint-enable no-unused-vars */ import injectTapEventPlugin from "react-tap-event-plugin"
-injectTapEventPlugin()
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
-import getMuiTheme from "material-ui/styles/getMuiTheme"
-import { muiTheme } from "./theme"
+import React from "react";
+/* eslint-enable no-unused-vars */ import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import { muiTheme } from "./theme";
 // import { goToAnchor } from "react-scrollable-anchor" "material-ui/BottomNavigation"
-import Drawer from "material-ui/Drawer"
-import MenuItem from "material-ui/MenuItem"
-import { StickyContainer, Sticky } from "react-sticky"
+import Drawer from "material-ui/Drawer";
+import MenuItem from "material-ui/MenuItem";
 
-import "./prism.css"
-import "./index.css"
+import "./prism.css";
+import "./index.css";
 import {
   Introduction,
   Setup,
@@ -22,12 +21,12 @@ import {
   Examples,
   API,
   Notes
-} from "./Sections"
+} from "./Sections";
 
 class Docs extends React.Component {
   state = {
     open: false
-  }
+  };
 
   render() {
     const sections = [
@@ -40,26 +39,26 @@ class Docs extends React.Component {
       { id: "in-practice", name: "In Practice" },
       { id: "examples", name: "Examples" },
       { id: "notes", name: "Notes" }
-    ]
+    ];
 
     const list = sections.map(d => (
       <p>
         <a href={"#" + d.id}>{d.name}</a>
       </p>
-    ))
+    ));
 
     const drawerSections = sections.map(d => {
       return (
         <MenuItem
           onTouchTap={() => {
-            this.setState({ open: false })
-            window.location = `#${d.id}`
+            this.setState({ open: false });
+            window.location = `#${d.id}`;
           }}
         >
           {d.name}
         </MenuItem>
-      )
-    })
+      );
+    });
 
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
@@ -69,7 +68,7 @@ class Docs extends React.Component {
               <img
                 src="img/menu.png"
                 onClick={() => {
-                  this.setState({ open: !this.state.open })
+                  this.setState({ open: !this.state.open });
                 }}
               />react-annotation
             </h1>
@@ -81,52 +80,50 @@ class Docs extends React.Component {
           >
             {drawerSections}
           </Drawer>
-          <StickyContainer>
-            <div className="container-fluid main">
-              <div className="row">
-                <div className="col-sm-10 col-sm-offset-1 col-xs-12 smallHeader">
-                  <header className="box-row">
-                    <h1 className="hide-xs">react-annotation</h1>
+          <div className="container-fluid main">
+            <div className="row">
+              <div className="col-sm-10 col-sm-offset-1 col-xs-12 smallHeader">
+                <header className="box-row">
+                  <h1 className="hide-xs">react-annotation</h1>
 
-                    <p>
-                      Made with <img src="img/heart.png" /> by
-                      <a href="http://www.susielu.com"> Susie Lu</a>
-                    </p>
-                  </header>
+                  <p>
+                    Made with <img src="img/heart.png" /> by
+                    <a href="http://www.susielu.com"> Susie Lu</a>
+                  </p>
+                </header>
 
-                  <div className="row">
-                    <div className="col-sm-3 col-xs-12 hide-xs">
-                      <Sticky>{list}</Sticky>
-                    </div>
-                    <div className="col-sm-9 col-xs-12">
-                      <Introduction />
-                      <Setup />
-                      <Types />
-                      <AnnotationTypesAPI />
-                      <ExtendingTypes />
-                      <API />
-                      <InPractice />
-                      <Examples />
-                      <Notes />
-                    </div>
+                <div className="row">
+                  <div className="col-sm-3 col-xs-12 hide-xs sticky-menu">
+                    {list}
                   </div>
-                </div>
-                <div className="col-xs-10 col-xs-offset-1">
-                  <div className="row">
-                    <footer className="col-xs-9 col-xs-offset-3">
-                      <small>
-                        Made by <a href="http://www.susielu.com">Susie Lu</a>
-                      </small>
-                    </footer>
+                  <div className="col-sm-9 col-xs-12">
+                    <Introduction />
+                    <Setup />
+                    <Types />
+                    <AnnotationTypesAPI />
+                    <ExtendingTypes />
+                    <API />
+                    <InPractice />
+                    <Examples />
+                    <Notes />
                   </div>
                 </div>
               </div>
+              <div className="col-xs-10 col-xs-offset-1">
+                <div className="row">
+                  <footer className="col-xs-9 col-xs-offset-3">
+                    <small>
+                      Made by <a href="http://www.susielu.com">Susie Lu</a>
+                    </small>
+                  </footer>
+                </div>
+              </div>
             </div>
-          </StickyContainer>
+          </div>
         </div>
       </MuiThemeProvider>
-    )
+    );
   }
 }
 
-export default Docs
+export default Docs;
