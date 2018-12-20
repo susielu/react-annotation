4add72587217e70c7ba3ada45d00c9338d64d50e
slu <slu@netflix.com>
2018-12-20 11:12:54 -0800

    remove cypress

--- cypress/integration/sample_test.js
+++
@@ -1,7 +0,0 @@
-describe("My First Test", function() {
-  it("Visits the Kitchen Sink", function() {
-    cy.visit("./build/index.html")
-
-    cy.contains("Options").click()
-  })
-})
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path")
+const path = require("path");
 
 module.exports = {
   entry: "./src/components/index.js",
@@ -21,4 +21,4 @@ module.exports = {
     react: "React",
     "react-dom": "ReactDOM"
   }
-}
+};
